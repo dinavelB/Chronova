db28ccad6b12c5532f97a7783de4f8517d806773
dinavelB <binongo.dinavel.escodero@gmail.com>
2026-03-31 20:07:12 +0800

    added creds

--- chronova/lib/config/base-api.ts
+++ chronova/lib/config/base-api.ts
@@ -3,4 +3,7 @@ import axios from "axios";
 export const api = axios.create({
   baseURL: "http://localhost:4001/api/users",
   withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
